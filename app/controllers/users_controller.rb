373e354acf8dfaf11ea3b27b2d46aa7eedfd447a
rina <purotoinu@icloud.com>
2023-06-11 03:54:27 +0900

    Usersコントローラー作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+    skip_before_action :require_login, only: %i[new create]
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user =User.new(user_params)
+        @user.name ||= "Default Name"
+        if @user.save
+            redirect_to login_path, success: t('.success')
+        else
+            flash.now[:danger] = t('.fail')
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation)
+    end
+end
