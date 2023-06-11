6275b8bef40e63db155fc00f13eb6703928c5a79
rina <purotoinu@icloud.com>
2023-06-11 18:19:46 +0900

    バリデーション追加

--- app/models/movierecord.rb
+++ app/models/movierecord.rb
@@ -1,3 +1,7 @@
 class Movierecord < ApplicationRecord
   belongs_to :user
+
+  validates :title, presence: true, length: { maximum: 100 }
+  validates :genre, presence: true
+  validates :review, length: { maximum: 65_535 }
 end
