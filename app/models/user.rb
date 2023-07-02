31c3243c245ad2fa65e7835a00c5038904339dbb
rina <purotoinu@icloud.com>
2023-07-02 18:19:13 +0900

    モデル編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
 
   has_many :movierecords, dependent: :destroy
   has_many :likes, dependent: :destroy
-  has_many :likes_movierecords, through: :likes, source: :movierecord
+  has_many :like_movierecords, through: :likes, source: :movierecord
 
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
@@ -17,5 +17,17 @@ class User < ApplicationRecord
 
   def own?(object)
     id == object.user_id
+  end
+
+  def like(movierecord)
+    like_movierecords << movierecord
+  end
+
+  def unlike(movierecord)
+    like_movierecords.destroy(movierecord)
+  end
+
+  def like?(movierecord)
+    like_movierecords.include?(movierecord)
   end
 end
