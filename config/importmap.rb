6ad1f2f2656523c17db41f22d5cc9b25b1c30f05
rina <purotoinu@icloud.com>
2023-07-02 18:40:41 +0900

    確認アラート実装

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.3/dist/js/bootstrap.esm.js"
 pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.8/lib/index.js"
+pin_all_from "app/javascript/movierecord_confirm", under: "movierecord_confirm"
