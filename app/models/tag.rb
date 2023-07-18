8f06f306a46c42fa355655b27126188f31b98741
rina <purotoinu@icloud.com>
2023-07-19 01:09:44 +0900

    タグ機能実装と修正

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ApplicationRecord
-  has_many :movierecord_tags, dependent: :destroy
+  has_many :movierecord_tags, dependent: :destroy, foreign_key: 'tag_id'
   has_many :movierecords, through: :movierecord_tags
 
   validates :name, uniqueness: true, presence: true
