f90e92f5be7a85973e8dd1def32ce24d2790973b
rina <purotoinu@icloud.com>
2023-07-30 17:02:55 +0900

    ページタイトルを動的に表示する

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
-    def logged_in?
-        session[:user_id].present?
-      end
+  def logged_in?
+      session[:user_id].present?
+  end
+
+  def page_title(page_title = '')
+    base_title = 'My Cinema Record'
+
+    page_title.empty? ? base_title : page_title + " | " + base_title
+  end
 end
