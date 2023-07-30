f7c322d24171d5d8be18638c8a097d3b034290b9
rina <purotoinu@icloud.com>
2023-07-30 16:14:09 +0900

    like用を追加

--- app/javascript/movierecord_confirm.js
+++ app/javascript/movierecord_confirm.js
@@ -7,3 +7,20 @@ function check(){
       return false; 
     }
   }
+
+
+   // いいね関連
+  document.addEventListener('turbo:load', () => {
+    const likeButton = document.getElementById('like');
+  
+    likeButton.addEventListener('ajax:success', (event) => {
+      const [data, _status, xhr] = event.detail;
+      if (data.success) {
+        Turbo.visit(window.location, { action: "replace" }); // ページ全体を再読み込み
+      }
+    });
+  
+    likeButton.addEventListener('ajax:error', () => {
+      alert('エラーが発生しました');
+    });
+  });
