a8b5cddf662b189ba4e84b190eefccccc65a3b34
rina <purotoinu@icloud.com>
2023-08-07 01:57:40 +0900

    default_meta_tagsメソッドの定義

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,32 @@ module ApplicationHelper
 
     page_title.empty? ? base_title : page_title + " | " + base_title
   end
+
+  def default_meta_tags
+    {
+      site: 'MyCinemaRecord',
+      title: '見た映画を記録＆ツイートできるサービス',
+      reverse: true,
+      charset: 'utf-8',
+      description: '',
+      keywords: '映画,レビュー,感想,Twitterシェア,記録,',
+      canonical: request.original_url,
+      separator: '|',
+      og: {
+        site_name: :site,
+        title: :title,
+        description: :description,
+        type: 'website',
+        url: request.original_url,
+        image: image_url('ogp.png'), # 配置するパスやファイル名によって変更すること
+        local: 'ja-JP'
+      },
+      # Twitter用の設定を個別で設定する
+      twitter: {
+        card: 'summary_large_image', # Twitterで表示する場合は大きいカードにする
+        site: '@r_RUNTEQ37', # アプリの公式Twitterアカウントがあれば、アカウント名を書く
+        image: image_url('ogp.png') # 配置するパスやファイル名によって変更すること
+      }
+    }
+  end
 end
