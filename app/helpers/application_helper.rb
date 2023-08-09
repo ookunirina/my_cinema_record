ae70c736fc23f4732d1c4e6453036d6a5f8b5b63
rina <purotoinu@icloud.com>
2023-08-10 01:18:10 +0900

    ランク上位３名のみに色違い王冠

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,14 @@ module ApplicationHelper
 
     page_title.empty? ? base_title : page_title + " | " + base_title
   end
+
+  def top_ranking_crown(rank)
+    crown_icons = [
+      '<i class="fa fa-crown" style="color:#fff700"></i>',
+      '<i class="fa fa-crown" style="color:#CCCCCC"></i>',
+      '<i class="fa fa-crown" style="color:#c27100"></i>',
+    ]
+    crown_icons[rank].html_safe
+  end
+
 end
