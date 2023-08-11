320be7a06a1b00f229dda3bc9615ed8e93634568
rina <purotoinu@icloud.com>
2023-08-12 04:18:22 +0900

    修正

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,7 @@ class MoviesController < ApplicationController
   require 'dotenv/load'
   require 'themoviedb-api'
   
-  Tmdb::Api.key(d95117c30490d5092714acfecca3f9b2)
+  Tmdb::Api.key(ENV["TMDB_API_KEY"])
   Tmdb::Api.language("ja")
 
   def search
