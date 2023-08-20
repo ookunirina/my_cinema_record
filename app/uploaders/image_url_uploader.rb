125e1b2a60fb38436b33dd3abefdf87cf678fcb5
rina <purotoinu@icloud.com>
2023-08-21 01:46:15 +0900

    デフォルトの画像修正

--- app/uploaders/image_url_uploader.rb
+++ app/uploaders/image_url_uploader.rb
@@ -15,7 +15,7 @@ class ImageUrlUploader < CarrierWave::Uploader::Base
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
-    'movierecord_placeholder.jpg'
+    'default.png'
   end
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
