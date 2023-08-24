c58834a07284f8acace00952bb8241e53440d97e
rina <purotoinu@icloud.com>
2023-08-25 02:24:26 +0900

    LetterOpenerWebにアクセスするために必要な記述を追記

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
   # Defines the root path route ("/")
   root 'top#index'
   get 'login', to: 'user_sessions#new'
