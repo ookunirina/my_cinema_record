7f1695967eb3c5f3e9aad6e177cea9a97da9f7ae
rina <purotoinu@icloud.com>
2023-08-31 02:42:05 +0900

    修正

--- app/controllers/recommended_controller.rb
+++ app/controllers/recommended_controller.rb
@@ -1,6 +1,11 @@
 class RecommendedController < ApplicationController
-    def index
-        tag = Tag.find_by(name: 'オススメ')
-        @recommended_movierecords = Movierecord.joins(:tags).where(tags: { id: tag.id }).where.not(user_id: current_user.id).order("RANDOM()").limit(3)
-      end
+  def index
+    tag = Tag.find_by(name: 'オススメ')
+    if tag
+      @recommended_movierecords = Movierecord.joins(:tags).where(tags: { id: tag.id }).where.not(user_id: current_user.id).order("RANDOM()").limit(3)
+    else
+      # タグが見つからなかった場合の処理を記述
+      @recommended_movierecords = []
+    end
+  end
 end
