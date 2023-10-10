2f4e96025dc56ae40b57d87f4e0a34bc8a660477
rina <purotoinu@icloud.com>
2023-10-11 02:54:10 +0900

    enumの設定を追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,8 @@ class User < ApplicationRecord
   has_many :like_movierecords, through: :likes, source: :movierecord
   has_many :schedules
 
+  enum role: { general: 0, admin: 1 }
+
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
